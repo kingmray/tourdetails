7ec0d480f0428b2a70d5b2c8d8ad2826a1c82240
Raymond Schmidt <raymond.schmidt@googlemail.com>
2017-07-03 12:48:40 +0200

    added editing

--- tour.py
+++ tour.py
@@ -1,18 +1,28 @@
+from tinydb import TinyDB, Query
 from flask import Flask, request, send_from_directory, render_template
+
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
+db = TinyDB('./db.json')
 @app.route('/')
 def root():
-    return render_template('index.html')
+    posts = db.all()
+    return render_template('index.html', posts=list(reversed(posts)))
 
 @app.route('/edit')
 def edit():
-    return render_template('edit.html')
+    posts = db.all()
+    return render_template('edit.html', posts=posts, edit=True)
 
 @app.route('/post/save', methods=['GET', 'POST'])
 def save():
-    print('Go!')
+    post = request.get_json()
+    Post = Query()
+    if db.search(Post.id == post['id']):
+        db.update(post, Post.id == post['id'])
+    else:
+        db.insert(post)
     return 'hallo'
 
 @app.route('/img/<path:path>')
